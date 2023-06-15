5a947fffb93b4e6fa55ab04c71b3294c84f2dd9c
caiomp <caiomp@ufba.br>
2023-06-14 21:47:36 -0300

    Porjeto implementado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("91993836","Modelo Padrão","71",64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Nubank");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iPhone = new Iphone("91993836","Modelo Padrão","71",64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Banco Inter");
